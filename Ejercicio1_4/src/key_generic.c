ed6ad8c1c673015075100024d9005306ba7f9dcd
Fede Pacher <fede_pacher@hotmail.com>
2020-05-15 17:31:42 -0300

    resolucion en clase ejercicio 4

---
+++ Ejercicio1_4/src/key_generic.c
@@ -0,0 +1,113 @@
+/*
+ * key_generic.c
+ *
+ *  Created on: Apr 1, 2020
+ *      Author: fedepacher
+ */
+
+/*=====[Inclusion de su propia cabecera]=====================================*/
+
+#include "../../Ejercicio1_3_nTeclas/inc/key_generic.h"
+
+/*=====[Inclusiones de dependencias de funciones privadas]===================*/
+
+/*=====[Macros de definicion de constantes privadas]=========================*/
+
+/*=====[Macros estilo funcion privadas]======================================*/
+
+/*=====[Definiciones de tipos de datos privados]=============================*/
+
+// Tipo de datos que renombra un tipo basico
+
+// Tipo de datos de puntero a funcion
+
+// Tipo de datos enumerado
+
+// Tipo de datos estructua, union o campo de bits
+
+/*=====[Definiciones de Variables globales publicas externas]================*/
+fsmButtonState_t fsmButtonState;
+
+
+/*=====[Definiciones de Variables globales publicas]=========================*/
+
+/*=====[Definiciones de Variables globales privadas]=========================*/
+
+/*=====[Prototipos de funciones privadas]====================================*/
+
+/*=====[Implementaciones de funciones publicas]==============================*/
+
+
+void buttonPressed(button_led_t* config){
+	config->time_down = xTaskGetTickCount();
+}
+
+void buttonReleased(button_led_t* config){
+	config->time_up = xTaskGetTickCount();
+	if(config->time_up >= config->time_down)
+		config->messure_time = config->time_up - config->time_down;
+	else
+		config->messure_time = config->time_down - config->time_up ;
+
+}
+
+void fsmButtonInit(button_led_t* config){
+	config->fsmButtonState = STATE_BUTTON_UP;
+	config->countFalling = 0;
+	config->countRising = 0;
+}
+
+void fsmButtonError(button_led_t* config){
+	config->fsmButtonState = STATE_BUTTON_UP;
+}
+
+void fsmButtonUpdate(button_led_t* config) {
+
+	switch (config->fsmButtonState) {
+	case STATE_BUTTON_UP:
+
+		if (!gpioRead(config->button)) {
+			config->fsmButtonState = STATE_BUTTON_FALLING;
+		}
+
+		break;
+	case STATE_BUTTON_FALLING:
+		if (config->countFalling >= DEBOUNCE_TIME) {
+			if (!gpioRead(config->button)) {
+				config->fsmButtonState = STATE_BUTTON_DOWN;
+				buttonPressed(config);
+
+			} else{
+				config->fsmButtonState = STATE_BUTTON_UP;
+			}
+			config->countFalling = 0;
+		}
+		config->countFalling++;	//1 cycle = 1ms
+		break;
+	case STATE_BUTTON_DOWN:
+		if (gpioRead(config->button)) {
+			config->fsmButtonState = STATE_BUTTON_RISING;
+		}
+		break;
+	case STATE_BUTTON_RISING:
+		if (config->countRising >= DEBOUNCE_TIME) {
+			if (gpioRead(config->button)) {
+				config->fsmButtonState = STATE_BUTTON_UP;
+				buttonReleased(config);
+				} else{
+					config->fsmButtonState = STATE_BUTTON_DOWN;
+				}
+			config->countRising = 0;
+		}
+		config->countRising++;
+		break;
+	default:
+		fsmButtonError(config);
+		break;
+	}
+
+}
+
+
+/*=====[Implementaciones de funciones de interrupcion publicas]==============*/
+
